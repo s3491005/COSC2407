131567e8b0185021ab8a748e33a159e300f6d14d
s3491005 <s3491005@student.rmit.edu.au>
2018-03-24 20:12:11 +1100

    Connect to Db and populate rows

--- CSVParser.java
+++ CSVParser.java
@@ -2,16 +2,139 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSetMetaData;
 
 public class CSVParser {
 
+    private static final String FILENAME = "BUSINESS_NAMES_201803.csv";
+    private static final String DELIMITER = "\t";
+    private static final String BUSINESS_NAME_TABLE = "BUSINESS_NAME";
+    private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+    private static final String DB_URL = "jdbc:derby:BusinessNameDb";
+    
+    private static Connection connection = null;
+    private static Statement statement = null;
+    
+    private static void connect() {
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DB_URL); 
+            System.out.println("Connected to " + DB_URL);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private static void disconnect() {
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                DriverManager.getConnection(DB_URL + ";shutdown=true");
+                connection.close();
+            }           
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+        }
+    }
+
+    private static void deleteAllBusinessNames() {
+        try {
+            statement = connection.createStatement();
+            statement.execute("DELETE FROM " + BUSINESS_NAME_TABLE);
+            statement.close();
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+        }
+    }
+
+    public static String getValue(String value) {
+        if (value == null || value.isEmpty()) {
+            return "NULL";
+        }
+        return "'" + value + "'"; // wrap in single-quotes
+    }
+
+    private static String reformatDate(String date) {
+        if (date == null || date.isEmpty() || date.length() != 10) {
+            return "";
+        }
+        return date.substring(6) + "-" + date.substring(3,5) + "-" + date.substring(0,2);
+    }
+
+    public static String getFirstChar(String str) {
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+        if (str == "NT") {
+            return "O";
+        }
+        return str.substring(0,1);
+    }
+
+    public static String escapeSingleQuotes(String str) {
+        if (str == null || str.isEmpty() || !str.contains("'")) {
+            return str;
+        }
+        return Pattern.compile("\\'").matcher(str).replaceAll("''");
+    }
+
+    public static void insertBusinessName(int id, String name, String status, String regDate, String cancelDate, String renewDate, String stateNum, String state, String abn) {
+        try {
+            StringBuilder sql = new StringBuilder(); 
+            sql.append("insert into ");
+            sql.append(BUSINESS_NAME_TABLE);
+            sql.append(" values (");
+
+            sql.append(id);
+            sql.append(",");
+            sql.append(getValue(escapeSingleQuotes(name)));
+            sql.append(",");
+            sql.append(getValue(getFirstChar(status)));
+            sql.append(",");
+            sql.append(getValue(reformatDate(regDate)));
+            sql.append(",");
+            sql.append(getValue(reformatDate(cancelDate)));
+            sql.append(",");
+            sql.append(getValue(reformatDate(renewDate)));
+            sql.append(",");
+            sql.append(getValue(stateNum));
+            sql.append(",");
+            sql.append(getValue(getFirstChar(state)));
+            sql.append(",");
+            sql.append(getValue(abn));
+
+            sql.append(")");
+
+            System.out.println(sql.toString());
+
+            statement = connection.createStatement();
+            statement.execute(sql.toString());
+            statement.close();
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
 
-        final String FILENAME = "BUSINESS_NAMES_201803.csv";
-        final String DELIMITER = "\t";
         String line = "";
-        int row = 0;
-        final int MAX = 100000;
+        int row = -1;
+        final int MAX = 10;
         String[] cols;
         String name;
         String status;
@@ -22,67 +145,36 @@ public class CSVParser {
         String state;
         String abn;
 
-        HashSet<String> statusList = new HashSet<>();
-        HashSet<String> stateList   = new HashSet<>();
-        int minNameLength = 10000;
-        int maxNameLength = 0;
-        int nameLength;
+        connect();
+        deleteAllBusinessNames();
 
         try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
 
-            while ((line = br.readLine()) != null) {
+            while (row < MAX && (line = br.readLine()) != null) {
                 row++;
-                if (row == 1) {
+                if (row == 0) {
                     continue; // skip the header row
                 }
                 
                 cols = line.split(DELIMITER);
-                name       = cols[1];
-                status     = (cols.length > 2) ? cols[2] : "";
-                //regDate    = (cols.length > 3) ? reformatDate(cols[3]) : "";
-                //cancelDate = (cols.length > 4) ? reformatDate(cols[4]) : "";
-                //renewDate  = (cols.length > 5) ? reformatDate(cols[5]) : "";
-                //stateNum   = (cols.length > 6) ? cols[6] : "";
-                state      = (cols.length > 7) ? cols[7] : "";
-                //abn        = (cols.length > 8) ? cols[8] : "";
 
+                name       = (cols.length > 1) ? cols[1] : null;
+                status     = (cols.length > 2) ? cols[2] : null;
+                regDate    = (cols.length > 3) ? cols[3] : null;
+                cancelDate = (cols.length > 4) ? cols[4] : null;
+                renewDate  = (cols.length > 5) ? cols[5] : null;
+                stateNum   = (cols.length > 6) ? cols[6] : null;
+                state      = (cols.length > 7) ? cols[7] : null;
+                abn        = (cols.length > 8) ? cols[8] : null;
 
-                if (!statusList.contains(status)) {
-                    statusList.add(status);
-                }
-                if (!state.isEmpty() && !stateList.contains(state)) {
-                    stateList.add(state);
-                }
-
-                nameLength = name.length();
-                if (nameLength < minNameLength) {
-                    minNameLength = nameLength + 0;
-                }
-                if (nameLength > maxNameLength) {
-                    maxNameLength = nameLength + 0;
-                }
-
-                //System.out.println(name);
-                //System.out.println(status + "\t" + regDate + "\t" + cancelDate + "\t" + renewDate + "\t" + stateNum + "\t" + regState + "\t" + abn);
+                insertBusinessName(row, name, status, regDate, cancelDate, renewDate, stateNum, state, abn);
             }
-
-            System.out.println(row);            // 2,523,933
-            System.out.println(minNameLength);  // 1
-            System.out.println(maxNameLength);  // 199
-            System.out.println(statusList);     // [Deregistered, Registered]
-            System.out.println(stateList);      // [VIC, ACT, NSW, NT, TAS, QLD, WA, SA]
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-    }
-
-    private static String reformatDate(String date) {
-        if (date == null || date.isEmpty() || date.length() != 10) {
-            return "";
-        }
-        return date.substring(6) + "-" + date.substring(3,5) + "-" + date.substring(0,2);
+        disconnect();
     }
 
 }
