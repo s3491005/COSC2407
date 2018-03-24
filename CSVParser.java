ee5dbae8e7236b50ff294dedab81d7d897fe3227
s3491005 <s3491005@student.rmit.edu.au>
2018-03-24 14:12:54 +1100

    Collect Stats

--- CSVParser.java
+++ CSVParser.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class CSVParser {
 
@@ -10,7 +11,7 @@ public class CSVParser {
         final String DELIMITER = "\t";
         String line = "";
         int row = 0;
-        final int MAX = 1000;
+        final int MAX = 100000;
         String[] cols;
         String name;
         String status;
@@ -18,29 +19,58 @@ public class CSVParser {
         String cancelDate;
         String renewDate;
         String stateNum;
-        String regState;
+        String state;
         String abn;
+
+        HashSet<String> statusList = new HashSet<>();
+        HashSet<String> stateList   = new HashSet<>();
+        int minNameLength = 10000;
+        int maxNameLength = 0;
+        int nameLength;
 
         try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
 
-            while (row <= MAX && (line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
                 row++;
                 if (row == 1) {
                     continue; // skip the header row
                 }
+                
                 cols = line.split(DELIMITER);
                 name       = cols[1];
-                status     = cols[2];
-                regDate    = reformatDate(cols[3]);
-                cancelDate = reformatDate(cols[4]);
-                renewDate  = reformatDate(cols[5]);
-                stateNum   = (cols.length > 6) ? cols[6] : "";
-                regState   = (cols.length > 7) ? cols[7] : "";
-                abn        = (cols.length > 8) ? cols[8] : "";
+                status     = (cols.length > 2) ? cols[2] : "";
+                //regDate    = (cols.length > 3) ? reformatDate(cols[3]) : "";
+                //cancelDate = (cols.length > 4) ? reformatDate(cols[4]) : "";
+                //renewDate  = (cols.length > 5) ? reformatDate(cols[5]) : "";
+                //stateNum   = (cols.length > 6) ? cols[6] : "";
+                state      = (cols.length > 7) ? cols[7] : "";
+                //abn        = (cols.length > 8) ? cols[8] : "";
 
-                System.out.println(name);
-                System.out.println(status + "\t" + regDate + "\t" + cancelDate + "\t" + renewDate + "\t" + stateNum + "\t" + regState + "\t" + abn);
+
+                if (!statusList.contains(status)) {
+                    statusList.add(status);
+                }
+                if (!state.isEmpty() && !stateList.contains(state)) {
+                    stateList.add(state);
+                }
+
+                nameLength = name.length();
+                if (nameLength < minNameLength) {
+                    minNameLength = nameLength + 0;
+                }
+                if (nameLength > maxNameLength) {
+                    maxNameLength = nameLength + 0;
+                }
+
+                //System.out.println(name);
+                //System.out.println(status + "\t" + regDate + "\t" + cancelDate + "\t" + renewDate + "\t" + stateNum + "\t" + regState + "\t" + abn);
             }
+
+            System.out.println(row);            // 2,523,933
+            System.out.println(minNameLength);  // 1
+            System.out.println(maxNameLength);  // 199
+            System.out.println(statusList);     // [Deregistered, Registered]
+            System.out.println(stateList);      // [VIC, ACT, NSW, NT, TAS, QLD, WA, SA]
 
         } catch (IOException e) {
             e.printStackTrace();
